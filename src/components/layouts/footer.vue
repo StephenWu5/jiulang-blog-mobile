760324a7eea724322dc84639932ab702496c4e77
Your Name <981079142@qq.com>
2021-12-23 21:22:44 +0800

    写好了博客系统的页面底部信息

--- src/components/layouts/footer.vue
+++ src/components/layouts/footer.vue
@@ -1,34 +1,73 @@
 <script lang="ts">
+import { reactive, ref } from "vue";
 export default {
 	name: "Footer",
 	setup() {
-		return {};
+        let codeUrl = ref('../../assets/code.jpg');
+		let user = reactive({
+			wechat: "1234567890",
+			email: "1234567890@qq.com",
+		});
+		return {
+			user,
+            codeUrl,
+		};
 	},
 };
 </script>
-
 <template>
 	<div class="footer-wrap">
-		<div class="footer w1000">
-			<div class="qrcode"></div>
+		<div class="footer">
+			<div class="qrcode">
+				<img src="../../assets/code.jpg" alt="" />
+			</div>
 			<div class="practice-mode">
 				<div class="text">
-					<p class="title">大家好，欢迎来到我的博客</p>
+					<h4 class="title">大家好，欢迎来到我的博客</h4>
 					<p class="title">我的联系方式</p>
-					<p>微信<span class="WeChat">1234567890</span></p>
-					<p>邮箱<span class="email">1234567890@qq.com</span></p>
+					<p>
+						微信<span class="WeChat">{{ user.wechat }}</span>
+					</p>
+					<p>
+						邮箱<span class="email">{{ user.email }}</span>
+					</p>
 				</div>
 			</div>
 		</div>
 	</div>
 </template>
-
 <style lang="scss" scoped>
-div {
+.footer-wrap {
+	border-top: 1px solid #402863;
+}
+.footer {
+	padding: 40px 30px 80px;
+	position: relative;
+	overflow: auto;
+	height: 380px;
+}
+.footer .qrcode {
+	width: 240px;
+}
+.footer .practice-mode {
+	padding-top: 40px;
+}
+.footer .practice-mode img {
+	float: left;
+	padding-right: 28px;
+}
+.footer .practice-mode .text {
+	color: #3f2863;
+	float: left;
+}
+.footer .practice-mode h4.text {
 	font-size: 20px;
-	color: black;
-	p {
-		font-size: 41px;
-	}
+	line-height: 60px;
+}
+.footer .practice-mode .text p {
+	line-height: 32px;
+}
+.footer .practice-mode .text p span {
+	padding-left: 24px;
 }
 </style>
--- src/components/layouts/nav.vue
+++ src/components/layouts/nav.vue
@@ -1,19 +1,42 @@
 <script lang="ts">
 export default {
-    name: 'Nav',
-    setup() {
+	name: "Nav",
+	setup() {
 		return {};
 	},
-}
+};
 </script>
 
 <template>
-    <div>内容区</div>
+	<div class="header">
+		<div class="menu-btn">
+			<div class="menu"></div>
+		</div>
+		<h1 class="logo">
+			<a href="index.html">
+				<span>MYBLOG</span>
+				<img src="../res/img/logo.png" />
+			</a>
+		</h1>
+		<div class="nav">
+            <a href="index.html" class="active">首页</a>
+			<a href="index.html" class="active">文章</a>
+			<a href="about.html">关于</a>
+		</div>
+		<ul class="layui-nav header-down-nav">
+			<li class="layui-nav-item"><a href="whisper.html">首页</a></li>
+			<li class="layui-nav-item">
+				<a href="index.html" class="active">文章</a>
+			</li>
+			<li class="layui-nav-item"><a href="about.html">关于</a></li>
+		</ul>
+		<p class="welcome-text">欢迎来到<span class="name">小明</span>的博客~</p>
+	</div>
 </template>
 
 <style scoped>
 div {
-    font-size: 20px;
-    color: black;
+	font-size: 20px;
+	color: black;
 }
 </style>
