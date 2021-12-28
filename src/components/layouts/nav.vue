0b2bf5677646f8550a2f2cb579def2716d866b5f
Your Name <981079142@qq.com>
2021-12-28 21:25:45 +0800

    继续写头部的样式

--- src/components/layouts/nav.vue
+++ src/components/layouts/nav.vue
@@ -1,20 +1,41 @@
 <script lang="ts">
 import { reactive } from "@vue/reactivity";
-import { Popup } from "vant";
-import { Grid, GridItem } from "vant";
+import { Popup, Grid, GridItem, List, Cell } from "vant";
 export default {
 	name: "Nav",
 	components: {
 		[Popup.name]: Popup,
 		[Grid.name]: Grid,
 		[GridItem.name]: GridItem,
+        [List.name]: List,
+        [Cell.name]: Cell,
 	},
 	setup() {
 		const state = reactive({
-			showPopUp: true,
+			showPopUp: false,
+            menuList: [
+                {
+                    name: '首页',
+                    url: '/index',
+                },
+                 {
+                    name: '我的文章',
+                    url: '/article',
+                }
+            ]
 		});
+		// 显示进度条
+		const showNavBar = () => {
+			state.showPopUp = true;
+		};
+		// 跳转首页
+		const gotoIndex = () => {};
+        const onLoad = () => {};
 		return {
 			state,
+			showNavBar,
+			gotoIndex,
+            onLoad,
 		};
 	},
 };
@@ -25,14 +46,22 @@ export default {
 		<van-popup
 			v-model:show="state.showPopUp"
 			position="left"
-			:style="{ height: '100%', width: '30%' }"
-			>内容内容内容内容内容内容内容121
+			:style="{ height: '100%', width: '40%' }"
+		>
+			<van-list
+				:loading="false"
+				:finished="true"
+				finished-text=""
+				@load="onLoad"
+			>
+				<van-cell v-for="(item, index) in state.menuList" :key="index" :title="item.name" />
+			</van-list>
 		</van-popup>
 		<van-grid :border="false">
-			<van-grid-item icon="photo-o" text="首页" />
+			<van-grid-item icon="smile" text="首页" @click="gotoIndex" />
 			<van-grid-item icon="" text="" />
 			<van-grid-item icon="" text="" />
-			<van-grid-item icon="photo-o" text="导航" />
+			<van-grid-item icon="wap-nav" text="导航" @click="showNavBar" />
 		</van-grid>
 		<div class="menu-btn">
 			<div class="menu"></div>
@@ -43,19 +72,7 @@ export default {
 				<img src="../res/img/logo.png" />
 			</a>
 		</h1>
-		<div class="nav">
-			<a href="index.html" class="active">首页</a>
-			<a href="index.html" class="active">文章</a>
-			<a href="about.html">关于</a>
-		</div>
-		<ul class="layui-nav header-down-nav">
-			<li class="layui-nav-item"><a href="whisper.html">首页</a></li>
-			<li class="layui-nav-item">
-				<a href="index.html" class="active">文章</a>
-			</li>
-			<li class="layui-nav-item"><a href="about.html">关于</a></li>
-		</ul>
-		<p class="welcome-text">欢迎来到<span class="name">小名</span>的博客~</p>
+		<p class="welcome-text">欢迎来到<span class="name">豆腐的</span>的博客~</p>
 	</div>
 </template>
 
